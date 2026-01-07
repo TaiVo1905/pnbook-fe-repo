e14c1c1b01326bfc177ad0444572106649515e50
Duc Tai <voductaitxqt123@gmail.com>
2026-01-07 14:30:27 +0700

    fix(PNBOOK-48): remove conflicting func

--- src/features/post/components/CreatePostModal.tsx
+++ src/features/post/components/CreatePostModal.tsx
@@ -15,14 +15,6 @@ import type {
   CreatePostPayload,
 } from '../types/post.type';
 
-const generateUUID = (): string => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
-
 export const CreatePostModal = ({
   isOpen,
   onClose,
--- src/features/post/components/EditPostModal.tsx
+++ src/features/post/components/EditPostModal.tsx
@@ -17,14 +17,6 @@ import type {
   Post,
 } from '../types/post.type';
 
-const generateUUID = (): string => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
-
 interface EditPostModalProps {
   isOpen: boolean;
   onClose: () => void;
